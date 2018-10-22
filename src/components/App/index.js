dcbb373f9cfbeed2a49ef7c9790c9cb6f905d962
Chris Chateau <christopherchateau@gmail.com>
2018-10-22 13:35:47 -0600

    Create file tree, header component, install react router and proptypes

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import { Route } from "react-router-dom";
+import Header from '../Header'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
       </div>
     );
   }
