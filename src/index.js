8de501dd64e017f6a40ec90f80aff9c48ce2a6fa
Chris Chateau <christopherchateau@gmail.com>
2018-10-24 13:09:15 -0600

    Add basic redux folders and files

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom'
-import './index.css';
-import App from './components/App';
-import * as serviceWorker from './serviceWorker';
+import "./index.css";
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import { render } from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import rootReducer from "../src/reducers";
+import App from "./components/App";
 
-const router = (
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-)
+const devTools =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const store = createStore(rootReducer, devTools);
 
-ReactDOM.render(router, document.getElementById('root'));
-
-serviceWorker.unregister();
+render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById("root")
+);
