bd77560a04ae1f059185054df20ace1962926dae
Chris Chateau <christopherchateau@gmail.com>
2018-10-28 15:17:34 -0600

    Add errorMessageReducer

---
+++ src/reducers/errorMessageReducer.js
@@ -0,0 +1,8 @@
+export const errorMessageReducer = (state = '', action) => {
+  switch (action.type) {
+    case "SET_ERROR_MESSAGE":
+      return action.errorMessage;
+    default:
+      return state;
+  }
+};
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from "redux";
 import { moviesReducer } from "./moviesReducer";
 import { loginReducer } from "./loginReducer";
 import { userReducer } from "./userReducer";
+import { errorMessageReducer } from "./errorMessageReducer";
 
 const rootReducer = combineReducers({
   movies: moviesReducer,
   loggedIn: loginReducer,
   currentUser: userReducer,
+  errorMessage: errorMessageReducer
 });
 
 export default rootReducer;
