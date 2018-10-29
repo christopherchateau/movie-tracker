bbe0a80c2f730d6446842963b116466580dc5b1f
Tobin Nelson <tobin.j.n@gmail.com>
2018-10-28 19:52:17 -0600

    Test fetchLoginuser

--- src/utilities/test/fetch.test.js
+++ src/utilities/test/fetch.test.js
@@ -1,18 +1,54 @@
 /* eslint-disable */
 
 import React from 'react';
-import { fetchData } from '../fetch';
+import * as Fetch from '../fetch';
+import * as Mocks from './mocks'
+
 
 describe('fetchData', () => {
-  it('should call fetchData with the correct params', async () => {
-    let mockUrl = ''
-    let mockData = {crew: [{name: 'Joel', job: 'Director'}]}
+  it('should call fetch with the correct params', async () => {
+    // let mockData = {crew: [{name: 'Joel', job: 'Director'}]}
     window.fetch = jest.fn().mockImplementation(() => 
-        Promise.resolve({json: () => Promise.resolve({mockData})})
+        Promise.resolve({json: () => Promise.resolve(Mocks.mockResults)})
       );
     const expected = "https://api.themoviedb.org/3/person/1223/movie_credits?api_key=9954e71d12ad27a2cefac26f2e808e76"
 
-    await fetchData(mockUrl)
+    Fetch.fetchData()
     expect(window.fetch).toHaveBeenCalledWith(expected)
   })
 })
+
+  it('calls fetch with the correct params when a user logs in', () => {
+    window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+      json: () => Promise.resolve(Mocks.mockUserLoginResponse)
+    }));
+
+    const mockEmail = Mocks.mockUserLogin.email
+    const mockPassword = Mocks.mockUserLogin.password
+    const expectedFetchBody = {
+      method: "POST",
+      credentials: "same-origin",
+      body: JSON.stringify({
+        email: mockEmail,
+        password: mockPassword
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+
+    Fetch.fetchLoginUser(mockEmail, mockPassword)
+    expect(window.fetch).toHaveBeenCalledWith("http://localhost:3000/api/users", expectedFetchBody)
+  })
+
+  it('calls fetch with the correct params when a user signsup', () => {
+  })
+
+  it('calls fetch with the correct params when a user adds a favorite', () => {
+  })
+
+  it('calls fetch with the correct params when retrieving favorites', () => {
+  })
+
+  it('calls fetch with the correct params when user removes a favorite', () => {
+  })
