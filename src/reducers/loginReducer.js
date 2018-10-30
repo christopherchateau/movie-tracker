3e6fdecb700e9f5a278c8400c5cbd58a28af5a97
Haley Jacobs <haleyljacobs@gmail.com>
2018-10-30 14:52:37 -0600

    Deleted SAVE_NAME from loginReducer

--- src/reducers/loginReducer.js
+++ src/reducers/loginReducer.js
@@ -2,8 +2,6 @@ export const loginReducer = (state = false, action) => {
   switch (action.type) {
     case "TOGGLE_LOGIN":
       return action.loggedIn;
-    case "SAVE_NAME":
-      return action.currentUser;
     default:
       return state;
   }
