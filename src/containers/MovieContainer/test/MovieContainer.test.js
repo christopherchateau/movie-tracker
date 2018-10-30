8b5dc869ea625167075ceee525db1c96e4d38514
Tobin Nelson <tobin.j.n@gmail.com>
2018-10-30 14:26:01 -0600

    Create mocks for MovieContainer

--- src/containers/MovieContainer/test/MovieContainer.test.js
+++ src/containers/MovieContainer/test/MovieContainer.test.js
@@ -4,28 +4,15 @@ import React from "react";
 import { shallow } from "enzyme";
 import { mapStateToProps, MovieContainer } from "../index";
 import Movie from "../../../containers/Movie";
+import * as Mocks from './mocks'
 
 describe("MovieContainer", () => {
   let wrapper;
   let mockMovies
 
   beforeEach(() => {
-    mockMovies = [
-      {
-        title: "The Big Lebowski",
-        date: "1998-03-06",
-        poster:
-          "https://image.tmdb.org/t/p/w600_and_h900_bestv2/aHaVjVoXeNanfwUwQ92SG7tosFM.jpg",
-        overview:
-          'Jeffrey "The Dude" Lebowski, a Los Angeles slacker who only wants to bowl and drink white Russians, is mistaken for another Jeffrey Lebowski, a wheelchair-bound millionaire, and finds himself dragged into a strange series of events involving nihilists, adult film producers, ferrets, errant toes, and large sums of money.',
-        voteAverage: 7.9,
-        id: 115,
-        favorited: false
-      }
-    ];
-
     wrapper = shallow(<MovieContainer 
-      movies={mockMovies}
+      movies={Mocks.mockMovies}
       showFavorites={true}
       userName={'Taylor'} 
     />);
@@ -39,9 +26,17 @@ describe("MovieContainer", () => {
     expect(wrapper).toMatchSnapshot();
   });
 
-  it("should render cards if there are movies in the movies array", () => {
+  it('should render image if nothing in favorites', () => {
+    expect(wrapper.find('.walter')).toBeDefined();
+  })
 
-    expect(wrapper.find(Movie)).toBeDefined();
+  it("should render cards if there are movies in the movies array", () => {
+    wrapper = shallow(<MovieContainer 
+      movies={Mocks.mockMovies}
+      showFavorites={false}
+      userName={'Taylor'} 
+    />);
+    expect(wrapper.find(Movie).length).toEqual(2);
   });
 
   it("should render the spinner image if there are no movies in the movies array", () => {
@@ -76,24 +71,4 @@ describe("mapStateToProps", () => {
     const mappedProps = mapStateToProps(mockState);
     expect(mappedProps).toEqual(expected);
   });
-  // it("should return an object with showFavorites", () => {
-  //   const mockState = {
-  //   };
-
-  //   const expected = {
-  //   };
-
-  //   const mappedProps = mapStateToProps(mockState);
-  //   expect(mappedProps).toEqual(expected);
-  // });
-  // it("should return an object with a userName", () => {
-  //   const mockState = {
-  //   };
-
-  //   const expected = {
-  //   };
-
-  //   const mappedProps = mapStateToProps(mockState);
-  //   expect(mappedProps).toEqual(expected);
-  // });
 });
---
+++ src/containers/MovieContainer/test/mocks.js
@@ -0,0 +1,24 @@
+export const mockMovies = [
+  {
+    title: "The Big Lebowski",
+    date: "1998-03-06",
+    poster:
+      "https://image.tmdb.org/t/p/w600_and_h900_bestv2/aHaVjVoXeNanfwUwQ92SG7tosFM.jpg",
+    overview:
+      'Jeffrey "The Dude" Lebowski, a Los Angeles slacker who only wants to bowl and drink white Russians, is mistaken for another Jeffrey Lebowski, a wheelchair-bound millionaire, and finds himself dragged into a strange series of events involving nihilists, adult film producers, ferrets, errant toes, and large sums of money.',
+    voteAverage: 7.9,
+    id: 115,
+    favorited: false
+  },
+  {
+    title: "The Big Lebowski",
+    date: "1998-03-06",
+    poster:
+      "https://image.tmdb.org/t/p/w600_and_h900_bestv2/aHaVjVoXeNanfwUwQ92SG7tosFM.jpg",
+    overview:
+      'Jeffrey "The Dude" Lebowski, a Los Angeles slacker who only wants to bowl and drink white Russians, is mistaken for another Jeffrey Lebowski, a wheelchair-bound millionaire, and finds himself dragged into a strange series of events involving nihilists, adult film producers, ferrets, errant toes, and large sums of money.',
+    voteAverage: 7.9,
+    id: 115,
+    favorited: false
+  }
+];
