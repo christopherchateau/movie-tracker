5cd385a697f0ec03e4618a3203c7b3e1d46246a1
Chris Chateau <christopherchateau@gmail.com>
2018-10-29 18:40:17 -0600

    Add favoritesReducer and action, add to rootReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,12 +3,14 @@ import { moviesReducer } from "./moviesReducer";
 import { loginReducer } from "./loginReducer";
 import { userReducer } from "./userReducer";
 import { errorMessageReducer } from "./errorMessageReducer";
+import { favoritesReducer } from "./favoritesReducer";
 
 const rootReducer = combineReducers({
   movies: moviesReducer,
   loggedIn: loginReducer,
   currentUser: userReducer,
-  errorMessage: errorMessageReducer
+  errorMessage: errorMessageReducer,
+  displayFavorites: favoritesReducer
 });
 
 export default rootReducer;
