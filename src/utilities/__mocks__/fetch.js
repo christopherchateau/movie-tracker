f038839198e78192e634130e9337144ae405fa01
Tobin Nelson <tobin.j.n@gmail.com>
2018-10-29 20:24:28 -0600

    Build out mock functions in __mocks__ for fetch

--- src/utilities/__mocks__/fetch.js
+++ src/utilities/__mocks__/fetch.js
@@ -1,3 +1,11 @@
-export const removeFavorite = jest.fn()
+export const fetchData = jest.fn()
+
+export const fetchLoginUser = jest.fn()
+
+export const FetchSignupUser = jest.fn()
 
 export const fetchAddFavorite = jest.fn()
+
+export const retrieveUserFavorites = jest.fn()
+
+export const removeFavorite = jest.fn()
